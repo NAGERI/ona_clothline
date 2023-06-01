ada73d79c13e91cc342dbca3076db00f80f5f058
nageri <onacedricnageri@gmail.com>
2023-06-01 15:03:21 +0300

    selector-memozation

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,20 @@
-export const selectorCategories = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectorCategoriesReducer = (state) => state.categories; // from our categories reducer.
+// the above selector is the one which runs, the 2 below are memoised.
+export const selectorCategories = createSelector(
+  [selectorCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+// the above is memonized
+
+export const selectorCategoriesMap = createSelector(
+  [selectorCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
 // Data transformation
